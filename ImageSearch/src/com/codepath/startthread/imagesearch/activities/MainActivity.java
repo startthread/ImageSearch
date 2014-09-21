6adae1bbcd550658b5fa008e8e07fdc613147081
Prakash Joshi <pjoshi@twitter.com>
2014-09-21 10:18:24 -0700

    Added action bar sherlock. Search View.

--- ImageSearch/src/com/codepath/startthread/imagesearch/activities/MainActivity.java
+++ ImageSearch/src/com/codepath/startthread/imagesearch/activities/MainActivity.java
@@ -8,28 +8,33 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 import com.codepath.startthread.imagesearch.R;
 import com.codepath.startthread.imagesearch.adapters.ImageResultsAdapter;
 import com.codepath.startthread.imagesearch.models.ImageResult;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockFragmentActivity {
 	
 	private static final String TAG = "MainActivity";
 	private static final String QUERY_URL = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=";
 	private static final String PAGES_PARAM =  "&rsz=8";
+	
+	private static final int REQUEST_FILTER = 1;
 
-	private EditText etQuery;
 	private GridView gvResults;
 	private List<ImageResult> imageResults;
 	private ImageResultsAdapter mAdapter;
@@ -46,7 +51,6 @@ public class MainActivity extends Activity {
 	}
 
 	private void setupViews() {
-		etQuery = (EditText) findViewById(R.id.etQuery);
 		gvResults = (GridView) findViewById(R.id.gvResults);
 		
 		gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -58,16 +62,6 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
-
-		// Search button click handler
-		findViewById(R.id.btnSearch).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						final String query = etQuery.getText().toString();
-						requestImages(query);
-					}
-				});
 	}
 	
 	private void requestImages(String search) {
@@ -100,4 +94,41 @@ public class MainActivity extends Activity {
 			
 		});
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getSupportMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    
+	    MenuItem searchItem = menu.findItem(R.id.action_search);	    
+	    SearchView searchView = (SearchView) searchItem.getActionView();
+	    searchView.setQueryHint(getResources().getString(R.string.image_search_hint));
+	    //searchView.setIconifiedByDefault(false);
+	    searchView.setOnQueryTextListener(new OnQueryTextListener() {
+	       @Override
+	       public boolean onQueryTextSubmit(String query) {
+	    	   	requestImages(query);
+	            return true;
+	       }
+
+	       @Override
+	       public boolean onQueryTextChange(String newText) {
+	           return false;
+	       }
+	   });
+	    
+	   return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_filter:
+				final Intent intent = new Intent(this, SearchFiltersActivity.class);
+				startActivityForResult(intent, REQUEST_FILTER);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
